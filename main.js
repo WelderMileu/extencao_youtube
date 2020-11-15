0bd07daabb8c2720fe501d5613cc8076e5b1c965
Welder Mileu <welderfelix21@gmail.com>
2020-11-15 10:25:42 -0300

    alterando o manifest e o codigo main

--- main.js
+++ main.js
@@ -2,24 +2,25 @@ const barraDeMenu = document.querySelector("#container");
 const input = document.querySelector("form #container #search-input input");
 const form = document.querySelector("#search-form")
 
+const canais = [
+	'-- Selecionar canal --',
+	'Pamela Drudi',
+	'Lucas Lira',
+	'Codigo Fonte Tv',
+	'Roketseat',
+	'Vlog da Si'
+]
+
 const interval = setInterval(() => {
 	if(barraDeMenu) {
-		clearInterval(interval)
-
-		const canais = [
-			'-- Selecionar canal --',
-			'Pamela Drudi',
-			'Lucas Lira',
-			'Codigo Fonte Tv',
-			'Roketseat',
-			'Vlog da Si'
-		]
+		clearInterval(interval)		
 
 		const select = document.createElement('select')
 		select.classList.add('form-control')
 		
 		canais.forEach(item => {
 			const option = document.createElement('option')
+			
 			option.innerHTML = item;
 			option.value = item;
 			select.appendChild(option)
@@ -34,4 +35,7 @@ const interval = setInterval(() => {
 		})	
 	}
 }, 100)
+
+
+
 
